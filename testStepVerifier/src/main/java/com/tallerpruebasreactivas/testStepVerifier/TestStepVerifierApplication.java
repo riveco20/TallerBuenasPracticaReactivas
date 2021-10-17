9b71043067e7624e1826f5b0277365176c58d3cb
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-10-17 10:44:09 -0500

    Montando Archivo Inicial

---
+++ testStepVerifier/src/main/java/com/tallerpruebasreactivas/testStepVerifier/TestStepVerifierApplication.java
@@ -0,0 +1,13 @@
+package com.tallerpruebasreactivas.testStepVerifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestStepVerifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestStepVerifierApplication.class, args);
+	}
+
+}
---
+++ testStepVerifier/src/test/java/com/tallerpruebasreactivas/testStepVerifier/TestStepVerifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tallerpruebasreactivas.testStepVerifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestStepVerifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
